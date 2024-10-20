43ce30ea4c104c343c8a12345f783819e85451d2
dreygrr <dreygrr@gmail.com>
2024-10-20 01:00:10 -0300

    spline adjustments

--- loginapp_front/src/components/dashboard/Book.tsx
+++ loginapp_front/src/components/dashboard/Book.tsx
@@ -1,10 +1,24 @@
 import Spline from '@splinetool/react-spline';
+import { useRef, useState } from 'react';
 
 export default function Book() {
+  const [bookState] = useState('baseState');
+  const splineAppRef = useRef(null);
+
+  const onLoad = (splineApp) => {
+    splineAppRef.current = splineApp;
+
+    const book = splineApp.findObjectByName('book');
+
+    console.log(book);
+    console.log(bookState);
+  };
+
   return (
     <>
       <Spline
-        scene="https://prod.spline.design/VCKMEE8rLn44oGiO/scene.splinecode" 
+        scene="https://prod.spline.design/VCKMEE8rLn44oGiO/scene.splinecode"
+        onLoad={onLoad}
       />
     </>
   );
